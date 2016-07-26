b1eef668efc53d19e754aec13b37ad5118425302
Adam Le <adamsuper169@gmail.com>
2016-07-26 11:20:43 +0700

    First commit

--- src/REDUP/Main.php
+++ src/REDUP/Main.php
@@ -2,5 +2,30 @@
 
 namespace REDUP
 
-use pocketmine\event\player\PlayerJoinEvent
-use pocketmine\event\player\PlayerDeathEvent
+/*Player Event*/
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
+/*Command Event*/
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandReader;
+use pocketine\command\CommandExecuter;
+use pocketmine\command\ConsoleCommandSender;
+/*Utils Event*/
+use pocketmine\utils\TextFormat as TF;
+/*Enchantment Event*/
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentList;
+/*Plugin Event*/
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+  public function onEnable(){
+    $this->getLogger()->info("REDUP đã hoạt động!");
+  }
+  public function onDisable(){
+    $this->getLogger()->info("REDUP đã dừng!");
+  }
+}
+
